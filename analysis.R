3e0f52b483db2dfd0d57686202cf6eabadbd5207
Prachatorn <46550708+Prachatorn@users.noreply.github.com>
2019-02-24 19:08:30 -0800

    Edited csv files and added analysis code

---
+++ analysis.R
@@ -0,0 +1,18 @@
+library(dplyr)
+
+arrival_relationships <- read.csv("Dataset/Refugees/arrival_relationships.csv",
+                                  stringsAsFactors = F)
+asylum_relationships <- read.csv("Dataset/Refugees/asylum_relationships.csv",
+                                 stringsAsFactors = F)
+
+# colnames(data)[colnames(data)=="old_name"] <- "new_name"
+colnames(arrival_relationships)[colnames(arrival_relationships) == 
+                                  "ï..Characteristic"] <- "character"
+colnames(asylum_relationships)[colnames(asylum_relationships) == 
+                                 "ï..Characteristic"] <- "character"
+
+combined <- arrival_relationships %>%
+  left_join(asylum_relationships, by = "character")
+
+write.csv(combined, "Dataset/Refugees/combined_relationships.csv", 
+          row.names = F)
