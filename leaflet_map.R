e9204089aa36bec7b3b86a6f725d7eed7e32e854
somchai joemjumroon <Prachatornjoe@gmail.com>
2019-03-11 16:23:21 -0700

    Finished map

--- leaflet_map.R
+++ leaflet_map.R
@@ -1,8 +1,4 @@
 # load in package
-library(leaflet)
-library(maps)
-library(sp)
-library(rgdal)
 library(plotly)
 library(dplyr)
 
@@ -14,32 +10,76 @@ affirmative_asylum <- read.csv("Refugees/country_affirmative_asylum.csv",
 colnames(affirmative_asylum)[colnames(affirmative_asylum) == 
                                   "ï..Country"] <- "country"
 
-df <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv',
-               stringsAsFactors = F)
-
 affirmative <- affirmative_asylum %>%
   filter(code != "NA")
 
-# https://plot.ly/r/choropleth-maps/#world-choropleth-map
-# light grey boundaries
-l <- list(color = toRGB("grey"), width = 0.5)
-
-# specify map projection/options
-g <- list(
-  showframe = FALSE,
-  showcoastlines = FALSE,
-  projection = list(type = 'Mercator')
-)
-
-p <- plot_geo(affirmative) %>%
-  add_trace(
-    z = ~X2007, color = ~X2007, colors = 'Blues',
-    text = ~country, locations = ~code, marker = list(line = l)
-  ) %>%
-  layout(
-    title = "Affirmative_asylums to the US",
-    geo = g
+affirmative[affirmative == "-"] <- 0
+affirmative[affirmative == "D"] <- NA
+affirmative <- affirmative %>%
+  rename_at (
+    vars(starts_with("X")),
+    funs(sub("X", "year_", .))
   )
 
-p
+affirmative$year_2007 <- gsub(",", "", affirmative$year_2007)
+affirmative$year_2008 <- gsub(",", "", affirmative$year_2008)
+affirmative$year_2009 <- gsub(",", "", affirmative$year_2009)
+affirmative$year_2010 <- gsub(",", "", affirmative$year_2010)
+affirmative$year_2011 <- gsub(",", "", affirmative$year_2011)
+affirmative$year_2012 <- gsub(",", "", affirmative$year_2012)
+affirmative$year_2013 <- gsub(",", "", affirmative$year_2013)
+affirmative$year_2014 <- gsub(",", "", affirmative$year_2014)
+affirmative$year_2015 <- gsub(",", "", affirmative$year_2015)
+affirmative$year_2016 <- gsub(",", "", affirmative$year_2016)
 
+country <- affirmative %>%
+  select(
+    country
+  )
+num_affirmative <- data.matrix(affirmative[2:11])
+country <- country %>%
+  mutate(
+    year_2007 = num_affirmative[, 1],
+    year_2008 = num_affirmative[, 2],
+    year_2009 = num_affirmative[, 3],
+    year_2010 = num_affirmative[, 4],
+    year_2011 = num_affirmative[, 5],
+    year_2012 = num_affirmative[, 6],
+    year_2013 = num_affirmative[, 7],
+    year_2014 = num_affirmative[, 8],
+    year_2015 = num_affirmative[, 9],
+    year_2016 = num_affirmative[, 10],
+    code = affirmative[, 12]
+  )
+
+country <- data.frame(country)
+View(country)
+
+# https://plot.ly/r/choropleth-maps/#world-choropleth-map
+
+p <- plot_geo(country) %>%
+  add_trace(
+    z = ~year_2007,
+    color = ~year_2007,
+    colors = "Reds",
+    text = ~country, 
+    locations = ~code, 
+    marker = list(
+      line = list(
+        color = toRGB("black"), 
+        width = 0.5
+      )
+    )
+  ) %>%
+  colorbar(
+    title = '# Of People'
+  ) %>%
+  layout(
+    title = "Affirmative Asylums to the US From Around The World",
+    geo = list(
+      showframe = FALSE,
+      showcoastlines = TRUE,
+      projection = list(type = 'Mercator')
+    )
+  )
+p
