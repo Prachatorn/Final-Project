beb80e2b66d0b1a78608ce98bb5277e06e0b8a36
somchai joemjumroon <Prachatornjoe@gmail.com>
2019-03-12 00:08:15 -0700

    Worked on maps

--- leaflet_map.R
+++ leaflet_map.R
@@ -5,10 +5,11 @@ library(dplyr)
 # Load in dataset
 
 affirmative_asylum <- read.csv("Refugees/country_affirmative_asylum.csv",
-                  stringsAsFactors = F)
+  stringsAsFactors = F
+)
 
-colnames(affirmative_asylum)[colnames(affirmative_asylum) == 
-                                  "ï..Country"] <- "country"
+colnames(affirmative_asylum)[colnames(affirmative_asylum) ==
+  "ï..Country"] <- "country"
 
 affirmative <- affirmative_asylum %>%
   filter(code != "NA")
@@ -16,7 +17,7 @@ affirmative <- affirmative_asylum %>%
 affirmative[affirmative == "-"] <- 0
 affirmative[affirmative == "D"] <- NA
 affirmative <- affirmative %>%
-  rename_at (
+  rename_at(
     vars(starts_with("X")),
     funs(sub("X", "year_", .))
   )
@@ -32,12 +33,13 @@ affirmative$year_2014 <- gsub(",", "", affirmative$year_2014)
 affirmative$year_2015 <- gsub(",", "", affirmative$year_2015)
 affirmative$year_2016 <- gsub(",", "", affirmative$year_2016)
 
-country <- affirmative %>%
+affirmative_country <- affirmative %>%
   select(
     country
   )
+
 num_affirmative <- data.matrix(affirmative[2:11])
-country <- country %>%
+affirmative_country <- affirmative_country %>%
   mutate(
     year_2007 = num_affirmative[, 1],
     year_2008 = num_affirmative[, 2],
@@ -52,34 +54,110 @@ country <- country %>%
     code = affirmative[, 12]
   )
 
-country <- data.frame(country)
-View(country)
+affirmative_country <- data.frame(affirmative_country)
 
 # https://plot.ly/r/choropleth-maps/#world-choropleth-map
-
-p <- plot_geo(country) %>%
+p <- plot_geo(affirmative_country) %>%
   add_trace(
     z = ~year_2007,
     color = ~year_2007,
     colors = "Reds",
-    text = ~country, 
-    locations = ~code, 
+    text = ~country,
+    locations = ~code,
     marker = list(
       line = list(
-        color = toRGB("black"), 
+        color = toRGB("black"),
         width = 0.5
       )
     )
   ) %>%
   colorbar(
-    title = '# Of People'
+    title = "# Of People"
   ) %>%
   layout(
     title = "Affirmative Asylums to the US From Around The World",
     geo = list(
       showframe = FALSE,
       showcoastlines = TRUE,
-      projection = list(type = 'Mercator')
+      projection = list(type = "Mercator")
     )
   )
 p
+
+defensive_asylum <- read.csv("Refugees/country_defensive_asylum.csv",
+  stringsAsFactors = F
+)
+
+colnames(defensive_asylum)[colnames(defensive_asylum) ==
+  "ï..Country"] <- "country"
+
+defensive <- defensive_asylum %>%
+  filter(code != "NA")
+
+defensive[defensive == "-"] <- 0
+defensive[defensive == "D"] <- NA
+defensive <- defensive %>%
+  rename_at(
+    vars(starts_with("X")),
+    funs(sub("X", "year_", .))
+  )
+
+defensive$year_2007 <- gsub(",", "", defensive$year_2007)
+defensive$year_2008 <- gsub(",", "", defensive$year_2008)
+defensive$year_2009 <- gsub(",", "", defensive$year_2009)
+defensive$year_2010 <- gsub(",", "", defensive$year_2010)
+defensive$year_2011 <- gsub(",", "", defensive$year_2011)
+defensive$year_2012 <- gsub(",", "", defensive$year_2012)
+defensive$year_2013 <- gsub(",", "", defensive$year_2013)
+defensive$year_2014 <- gsub(",", "", defensive$year_2014)
+defensive$year_2015 <- gsub(",", "", defensive$year_2015)
+defensive$year_2016 <- gsub(",", "", defensive$year_2016)
+
+defensive_country <- defensive %>%
+  select(
+    country
+  )
+num_defensive <- data.matrix(defensive[2:11])
+defensive_country <- defensive_country %>%
+  mutate(
+    year_2007 = num_defensive[, 1],
+    year_2008 = num_defensive[, 2],
+    year_2009 = num_defensive[, 3],
+    year_2010 = num_defensive[, 4],
+    year_2011 = num_defensive[, 5],
+    year_2012 = num_defensive[, 6],
+    year_2013 = num_defensive[, 7],
+    year_2014 = num_defensive[, 8],
+    year_2015 = num_defensive[, 9],
+    year_2016 = num_defensive[, 10],
+    code = defensive[, 12]
+  )
+
+defensive_country <- data.frame(defensive_country)
+
+plot_geo(defensive_country) %>%
+  add_trace(
+    z = ~year_2007,
+    color = ~year_2007,
+    colors = "Blues",
+    text = ~country,
+    locations = ~code,
+    marker = list(
+      line = list(
+        color = toRGB("black"),
+        width = 0.5
+      )
+    )
+  ) %>%
+  colorbar(
+    title = "# Of People"
+  ) %>%
+  layout(
+    title = "Defensive Asylums to the US From Around The World",
+    geo = list(
+      showframe = FALSE,
+      showcoastlines = TRUE,
+      projection = list(type = "Mercator")
+    )
+  )
+
